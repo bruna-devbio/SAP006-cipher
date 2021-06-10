1221015bc0d659d34c3c7b2009c21e0fa7681556
Bruna Calixto <bruna.ppp8@gmail.com>
2021-06-10 10:02:00 -0300

    Estrutura do HTML, Layout e Manipulação da DOM (WIP)

--- src/cipher.js
+++ src/cipher.js
@@ -1,13 +1,81 @@
+
 const cipher = {
-  // ...
-};
+  //1) Criar a function e iniciar os arrays:
+  encode: function initCripto(messageValue, deslocValue) {
+    let textArray = [];
+    let deslocArray = [];
+    let textEnd = '';
+
+    /*2) Criar o FOR para percorrer a mensagem do usuário e dar um push com charCodeAt que irá
+     devolver o caracter com o seu código(ASC):
+    */
+    for (let i = 0; msg < messageValue.length; i++) {
+      textArray.push(messageValue.charCodeAt(i));
+    }
+    /*3) Depois de puxar o code, criar o FOR para percorrer esse code e 
+    avaliar com IF se ele está entre 65 e 90 (A a Z)
+    */
+    for (j = 0; j < textArray.length; j++) {
+      if (textArray[j] >= 65 && textArray[j] <= 90) {
+        /*
+        4) Se estiver nestas condições acima, então executa push
+         que irá mandar o caracter com o code pra dentro do meu array "textArray" e a fórmula do Daniel será aplicada
+        */
+        deslocArray.push(((textArray[j] - 65 + deslocValue) % 26) + 65);
+      } else if (textArray[j] >= 97 && textoArray[j] <= 122) {
+        deslocArray.push((((textArray[j] - 97 + deslocValue) % 26) + 97));
+      } else {
+        deslocArray.push(textArray[j]);
+      }
+
+    }
+    for (let h = 0; h < deslocArray.length; h++) {
+      textEnd += String.fromCharCode(deslocArray[h]);
+    }
+    return textEnd;
+ } }
 
 export default cipher;
 
-function cifra(string) {
-return
-}
 
-//((codigoDaLetraASC - cod1aletra + desloc) % tamDoAlfabeto) + cod1aLetra
 
-// codigoASC => codigo0a25 => desloco => giro => codigoASC
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1) Fazer uma split(messageValue) e transformar em um array de letras;
+
+//2) Fazer a função de encode;
+
+//3) Fazer a função de decode;
+
+/*
+ Observações:
+ Como cruzar as letras do alfabeto com as letras da message:
+ a) utilizando o charCodeAt
+ b) for
+ c) if e else
+
+ codigoDaLetraASC - cod1aletra + desloc) % tamDoAlfabeto) + cod1aLetra
+
+ codigoASC => codigo0a25 => desloco => giro => codigoASC
+*/
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
-/*
+
 import cipher from './cipher.js';
 
-console.log(
+const index = {};
+// 1)Pegar valor de entrada no inputs (texto e offset):
+
+let message = document.getElementById('msg');
+message.addEventListener('keypress', event => {
+    let messageValue = message.value;
+    console.log(messageValue)
+    document.getElementById("textend").innerHTML = initCripto(messageValue, deslocValue);
+    
+})
+
+let desloc = document.querySelector('#offset');
+desloc.addEventListener('keypress', event => {
+    let deslocValue = desloc.value; 
+    console.log(deslocValue);
+})
+
+/*
+2) Validar os valores dos inputs por meio de funções:
+a) input message: verificar se está vazio e verificar
+ o tipo(string); 
+b) input desloc: verificar se está vazio
+ e verificar o tipo(number);
+*/ 
+
+//3) Tornar buttons encode e decode funcionais:
+
+// Encode
+const buttonEncode = document.getElementById('criptografar');
+buttonEncode.addEventListener('click', function() {
+initCripto()
+})
+
+// Decode
+const buttonDecode = document.getElementById('descriptografar');
+buttonDecode.addEventListener('click', function(){
+    console.log('desclick')//Substituir pela função de decode
+})
+
+//4) Imprimir resultado(texto criptografado) no input "results":
 
 
 
-let message = "";
 
-function capture() {
-    message = document.getElementById('msg').value;
-    document.getElementById('write').innerHTML = message
-}
-*/
+
+
+export default index;
+
+
+
+
+
+ 
