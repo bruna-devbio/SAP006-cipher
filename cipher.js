2a3c25a5a55759682ab14b27d12d0ed44c94d411
Bruna Calixto <bruna.ppp8@gmail.com>
2021-06-16 14:01:25 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,64 @@
+
+const cipher = {
+  encode: function (desloc, messageValue) {
+    let messageCripto = "";
+
+    if ( typeof desloc !== "number" || typeof messageValue !== "string" ) {
+      throw new TypeError
+    }
+
+    for (let i = 0; i < messageValue.length; i++) {
+
+      let code = messageValue.charCodeAt(i);
+
+      if (code >= 65 && code <= 90) {
+        messageCripto += String.fromCharCode((code - 65 + desloc) % 26 + 65);
+      }
+      else if (code >= 33 && code <= 64) {
+        messageCripto += String.fromCharCode((code - 33 + desloc) % 33 + 33);
+      }
+    }
+    return messageCripto;
+  },
+  decode: function (desloc, messageValue) {
+    let messageCripto = "";
+
+    if ( typeof desloc !== "number" || typeof messageValue !== "string") {
+      throw new TypeError
+    }
+
+    for (let i = 0; i < messageValue.length; i++) {
+      let code = messageValue.charCodeAt(i);
+      if (code >= 65 && code <= 90) {
+        messageCripto += String.fromCharCode((code - 90 - desloc) % 26 + 90);
+      }
+      else if (code >= 33 && code <= 64) {
+        messageCripto += String.fromCharCode((code - 64 - desloc) % 33 + 64);
+      }
+    }
+    return messageCripto;
+  }
+}
+export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,24 @@
+
+import cipher from './cipher.js';
+const btnCode = document.getElementById("criptografar");
+function cipherMsg() {
+    let desloc = Number(document.getElementById("offset").value);
+    if (desloc < 0) {
+        desloc = Math.abs(desloc);
+    }
+    let messageValue = document.getElementById("msg").value;
+    const cipherText = cipher.encode(desloc, messageValue);
+    document.getElementById("textEnd").value = cipherText;
+}
+btnCode.addEventListener("click", cipherMsg);
+
+const btnDecode = document.getElementById("descriptografar");
+function decipherMsg() {
+    let desloc = Number(document.getElementById("offset").value);
+    let messageValue = document.getElementById("msg").value; // = decipherMsg;
+    const decipherText = cipher.decode(desloc, messageValue);
+    document.getElementById("textEnd").value = decipherText;
+}
+btnDecode.addEventListener("click", decipherMsg);
+
+//console.log(cipher);
